e8a969715042687d670c77d386cbb866a0227e5d
Thomas Barrett <thomas.barrett@yourgolftravel.com>
2017-03-06 09:43:52 +0000

    Adde data values - but should do simpler verson with IDs

--- data.sql
+++ data.sql
@@ -2,10 +2,6 @@ BEGIN;
 
 DROP TABLE IF EXISTS people, languages, likes CASCADE;
 
-DECLARE
-  last_person_id   people.id%TYPE;
-  last_language_id languages.id%TYPE;
-
 CREATE TABLE people (
   id            serial        PRIMARY KEY,
   first_name    varchar(100)  NOT NULL,
@@ -17,12 +13,12 @@ CREATE TYPE context AS ENUM ('client', 'server', 'full-stack');
 CREATE TABLE languages (
   id      serial       PRIMARY KEY,
   name    varchar(100) NOT NULL,
-  context context      NOT NULL,
+  context context      NOT NULL
 );
 
 CREATE TABLE likes (
   person_id   integer REFERENCES people(id) ON UPDATE CASCADE,
-  language_id integer REFERENCES language(id) ON UPDATE CASCADE,
+  language_id integer REFERENCES languages(id) ON UPDATE CASCADE,
   CONSTRAINT  like_pk PRIMARY KEY (person_id, language_id)
 );
 
@@ -31,7 +27,7 @@ INSERT INTO people(first_name, surname) VALUES
   ('Tom', 'Barrett'),
   ('Steve', 'Hopkinson'),
   ('Emily', 'Bertwistle')
-RETURNING ID INTO last_person_id;
+RETURNING ID;
 
 INSERT INTO languages(name, context) VALUES
   ('Python', 'server'),
@@ -40,6 +36,22 @@ INSERT INTO languages(name, context) VALUES
   ('Java', 'server'),
   ('Elm', 'client'),
   ('CSS', 'client')
-RETURNING ID INTO last_language_id;
+RETURNING ID;
+
+INSERT INTO likes(person_id, language_id)
+WITH
+  person AS ( SELECT id FROM people WHERE people.first_name = 'Shireen' ),
+  language AS ( SELECT id FROM languages WHERE languages.name = 'SQL')
+SELECT person.id, language.id
+FROM person, language;
+
+INSERT INTO likes (
+  SELECT
+    people.id AS people_id,
+    languages.id AS language_id
+  FROM people
+  JOIN (VALUES ('Tom', 'SQL')) vals ON people.first_name = vals.column1
+  JOIN languages ON vals.column2 = languages.name
+);
 
 COMMIT;
