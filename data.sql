de7050d0192244ff9bc82b5a276cd6908e22a511
Tom Barrett <tbtommyb@gmail.com>
2017-03-12 19:32:09 +0000

    Rewrote database setup. Needs to be tested properly

--- data.sql
+++ data.sql
@@ -1,57 +1,82 @@
 BEGIN;
 
-DROP TABLE IF EXISTS people, languages, likes CASCADE;
+DROP TABLE IF EXISTS people, languages, knows CASCADE;
+DROP TYPE IF EXISTS environment;
 
 CREATE TABLE people (
   id            serial        PRIMARY KEY,
   first_name    varchar(100)  NOT NULL,
-  surname       varchar(100)  NOT NULL
+  surname       varchar(100)  NOT NULL,
+  location      varchar(100)
 );
 
-CREATE TYPE context AS ENUM ('client', 'server', 'full-stack');
+CREATE TYPE environment AS ENUM ('client', 'server', 'full-stack');
 
 CREATE TABLE languages (
-  id      serial       PRIMARY KEY,
-  name    varchar(100) NOT NULL,
-  context context      NOT NULL
+  id           serial       PRIMARY KEY,
+  name         varchar(100) NOT NULL,
+  environment  environment  NOT NULL,
+  release_date date         NOT NULL      
 );
 
-CREATE TABLE likes (
+CREATE TABLE knows (
   person_id   integer REFERENCES people(id) ON UPDATE CASCADE,
   language_id integer REFERENCES languages(id) ON UPDATE CASCADE,
+  rating      integer NOT NULL,
   CONSTRAINT  like_pk PRIMARY KEY (person_id, language_id)
 );
 
-INSERT INTO people(first_name, surname) VALUES
-  ('Shireen', 'Zoaby'),
-  ('Tom', 'Barrett'),
-  ('Steve', 'Hopkinson'),
-  ('Emily', 'Bertwistle')
+INSERT INTO people(first_name, surname, location) VALUES
+  ('Sharon', 'Smith', 'Nazareth'),
+  ('Ted', 'Burns', 'London'),
+  ('Stephen', 'Wistle', NULL),
+  ('Amanda', 'Bertwistle', 'Nazareth'),
+  ('David', 'Grewal', NULL),
+  ('John', 'White', 'London'),
+  ('Peter', 'Hallam-Wistle', 'London'),
+  ('Paul', 'Jones', 'Nazareth')
 RETURNING ID;
 
-INSERT INTO languages(name, context) VALUES
-  ('Python', 'server'),
-  ('SQL', 'server'),
-  ('JavaScript', 'full-stack'),
-  ('Java', 'server'),
-  ('Elm', 'client'),
-  ('CSS', 'client')
+INSERT INTO languages(name, environment, release_date) VALUES
+  ('Python', 'server', '26-Jan-94'),
+  ('SQL', 'server', '01-Jun-79'),
+  ('JavaScript', 'full-stack', '18-Sep-95'),
+  ('Java', 'full-stack', '23-Jan-96'),
+  ('Elm', 'client', '01-Apr-12'),
+  ('CSS', 'client', '10-Oct-94'),
+  ('Ruby', 'server', '25-Dec-96'),
+  ('C++', 'server', '01-Oct-85'),
+  ('CoffeeScript', 'client', '24-Dec-09'),
+  ('Swift', 'full-stack', '02-Jun-14')
 RETURNING ID;
 
-INSERT INTO likes(person_id, language_id)
-WITH
-  person AS ( SELECT id FROM people WHERE people.first_name = 'Shireen' ),
-  language AS ( SELECT id FROM languages WHERE languages.name = 'SQL')
-SELECT person.id, language.id
-FROM person, language;
-
-INSERT INTO likes (
-  SELECT
-    people.id AS people_id,
-    languages.id AS language_id
-  FROM people
-  JOIN (VALUES ('Tom', 'SQL')) vals ON people.first_name = vals.column1
-  JOIN languages ON vals.column2 = languages.name
-);
+/* We are hard-coding ID values because we know the tables will be empty and can start from 1.
+   Don't do this! In Real Life you would write a script to build up the relations instead. */
+INSERT INTO knows(person_id, language_id, rating) VALUES
+  (1, 9, 7),
+  (1, 4, 9),
+  (1, 10, 3),
+  (1, 7, 2),
+  (2, 1, 1),
+  (3, 8, 8),
+  (3, 6, 4),
+  (3, 5, 8),
+  (3, 1, 2),
+  (3, 2, 9),
+  (4, 6, 7),
+  (4, 3, 5),
+  (5, 4, 9),
+  (5, 10, 5),
+  (5, 8, 6),
+  (6, 8, 8),
+  (6, 4, 5),
+  (7, 3, 0),
+  (7, 5, 1),
+  (7, 4, 2),
+  (7, 7, 7),
+  (8, 1, 9),
+  (8, 8, 5),
+  (8, 9, 7),
+  (8, 5, 5);
 
 COMMIT;
