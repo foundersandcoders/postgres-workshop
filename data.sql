3fe7aee6fb1478fcf0b3b505f66442212f91a146
Tom Barrett <tbtommyb@gmail.com>
2017-03-26 17:47:37 +0100

    Fixed syntax errors

--- data.sql
+++ data.sql
@@ -12,16 +12,16 @@ CREATE TABLE authors (
   location      varchar(100)
 );
 
+CREATE TABLE publishers (
+  id      serial       PRIMARY KEY,
+  name    varchar(100) NOT NULL
+);
+
 CREATE TABLE books (
   id           serial       PRIMARY KEY,
   name         varchar(100) NOT NULL,
   release_date date         NOT NULL,
   publisher    integer	    REFERENCES publishers(id) ON UPDATE CASCADE
-);
-
-CREATE TABLE publishers (
-  id      serial PRIMARY KEY,
-  name    varchar(100) NOT NULL,
 );
 
 CREATE TABLE book_authors (
@@ -41,10 +41,10 @@ INSERT INTO authors(first_name, surname, location) VALUES
 RETURNING ID;
 
 INSERT INTO publishers(name) VALUES
-  'The Big Publishing House',
-  'McGraw-Hill',
-  'No Starch Press',
-  'Mega Corp Ltd'
+  ('The Big Publishing House'),
+  ('McGraw-Hill'),
+  ('No Starch Press'),
+  ('Mega Corp Ltd')
 RETURNING ID;
 
 INSERT INTO books(name, release_date, publisher) VALUES
@@ -55,14 +55,14 @@ INSERT INTO books(name, release_date, publisher) VALUES
   ('Elm Street', '01-Apr-12', 4),
   ('CSS: Cansei', '10-Oct-94', 1),
   ('Ruby Gems', '25-Dec-96', 3),
-  ('C++', 'server', 1),
+  ('C++', '06-Jul-17', 1),
   ('CoffeeScript in Java', '24-Dec-09', 2),
   ('Swift in 10 Days', '02-Jun-14', 2)
 RETURNING ID;
 
 /* We are hard-coding ID values because we know the tables will be empty and can start from 1.
    Don't do this! In Real Life you would write a script to build up the relations instead. */
-INSERT INTO book_authors(book_id, person_id) VALUES
+INSERT INTO book_authors(book_id, author_id) VALUES
   (9, 7),
   (4, 4),
   (10, 3),
